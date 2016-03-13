4e70130a78f43ea42c7a856525a1ca60e70906ea
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2016-03-13 12:31:34 +0100

    Add session management.
    The session management is now included into the app in order to pass some hidden
    variables between forms of the same view. (Maybe new usages later)

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask.ext.login import login_user, logout_user, current_user, login_required, LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, session
+from flask.ext.session import Session
 
 app = Flask(__name__)
 
@@ -9,10 +11,14 @@ app.config.from_object('config')
 
 # Database Initialization
 db = SQLAlchemy(app)
+
 # Login manager init
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-from app import views, models
+# Session Manager Init
+sess = Session()
+sess.init_app(app)
 
+from app import views, models
