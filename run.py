d83d2b5be3a3f3986000c3e1797175f6e7bd17ad
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2017-08-06 12:17:05 +0200

    Run develop server in HTTPS mode.

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from cineapp import app, socketio
 
 # We need to run the application with SocketIO object
 # If not the apply doesn't work well on socket event detection
-socketio.run(app,debug=True,host="0.0.0.0")
+socketio.run(app,debug=True,host="0.0.0.0",keyfile='/usr/local/etc/letsencrypt/live/ares.ptitoliv.net/privkey.pem', certfile='/usr/local/etc/letsencrypt/live/ares.ptitoliv.net/fullchain.pem')
